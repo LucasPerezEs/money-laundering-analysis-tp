8b1090cd6aa04ba2bfde928abf42dc9618b1a22b
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-06-06 00:51:08 -0300

    Edges filter now can manage large dataset

---
+++ src/graph_edges_filter/disk_storage_management.py
@@ -0,0 +1,95 @@
+import heapq
+import os
+
+from pathlib import Path
+
+MAX_ELEMENTS_IN_RAM = 10000
+DATA_FILE = "data.txt"
+TEMP_DATA_FILE = "temp_data.txt"
+
+
+
+class DiskSet():
+
+    def __init__(self, id=None, encode_func=None, decode_func=None):
+        # Define RAM set
+        self._internal_set = set()
+
+        # Store serialization functions
+        self._encode_func = encode_func
+        self._decode_func = decode_func
+
+        # Define folder
+        if id is not None:
+            self._data_dir = Path(f"/tmp/storage_{id}")
+        else:
+            self._data_dir = Path(f"/tmp/storage")
+        os.makedirs(self._data_dir, exist_ok=True)
+
+        # Define file
+        self._data_file_path = self._data_dir.joinpath(DATA_FILE)
+        if not self._data_file_path.exists():
+            self._data_file_path.touch()
+        self._temp_data_file_path = self._data_dir.joinpath(TEMP_DATA_FILE)
+
+
+    def __del__(self):
+        del self._internal_set
+        self._data_file_path.unlink(missing_ok=True)
+        self._temp_data_file_path.unlink(missing_ok=True)
+        try:
+            self._data_dir.rmdir()
+        except:
+            pass
+
+
+    # Internal functions
+    def _add_lf_to_strings(self, elements):
+        for elem in elements:
+            yield elem + '\n'
+
+
+    def _apply_encode_function(self, elem):
+        return elem if self._encode_func is None else self._encode_func(elem)
+    
+
+    def _apply_decode_function(self, elem):
+        if elem.endswith('\n'):
+            elem = elem[:-1]
+        return elem if self._decode_func is None else self._decode_func(elem)
+
+
+    def _flush_set(self):
+        # Check if there are elements
+        if len(self._internal_set) == 0:
+            return
+
+        # Order set
+        ordered_set = list(self._internal_set)
+        ordered_set.sort()
+
+        # Open files and merge
+        with open(self._data_file_path, "r") as current_data, open(self._temp_data_file_path, "w") as new_data:
+            files_merger = heapq.merge(current_data, self._add_lf_to_strings(ordered_set))
+            new_data.writelines(files_merger)
+
+        # Delete temp file and rename it
+        os.replace(self._temp_data_file_path, self._data_file_path)
+
+        # Clear RAM set
+        self._internal_set.clear()
+
+    # Exposed functions
+    def add(self, element):
+        self._internal_set.add(self._apply_encode_function(element))
+        # Check if set is full
+        if len(self._internal_set) == MAX_ELEMENTS_IN_RAM:
+            self._flush_set()
+
+
+    def elements(self):
+        self._flush_set()
+
+        with open(self._data_file_path, "r") as data_file:
+            for line in data_file:
+                yield self._apply_decode_function(line)
--- src/graph_edges_filter/incoming_edges_filter.py
+++ src/graph_edges_filter/incoming_edges_filter.py
@@ -1,26 +1,82 @@
-import json
 import logging
 import os
 import zlib
-from collections import defaultdict
 from common.middleware.worker_base import WorkerBase
+from disk_storage_management import DiskSet
 
 class IncomingEdgesFilter(WorkerBase):
 
     def __init__(self):
         super().__init__()
-        self.min_incoming = int(os.environ.get("MIN_INCOMING", "0"))
+        self._min_incoming = int(os.environ.get("MIN_INCOMING", "5"))
+        self._clients_files_by_id = {}
 
-        self.unique_origins_by_client = defaultdict(lambda: defaultdict(set))
+    @staticmethod
+    def encode(elem):
+        return f"{elem['To Bank']}|{elem['Account.1']}|{elem['From Bank']}|{elem['Account']}"
 
-        self.tmp_dir = f"/tmp/inc_filter_{self.shard_id}"
-        os.makedirs(self.tmp_dir, exist_ok=True)
+    @staticmethod
+    def decode(elem):
+        split_elems = elem.split("|")
+        return {
+            "To Bank" : split_elems[0],
+            "Account.1" : split_elems[1],
+            "From Bank" : split_elems[2],
+            "Account" : split_elems[3]
+        }
 
     def process(self, data):
+        # Check client ID
+        client_id = data.get("client_id")
+        if client_id not in self._clients_files_by_id:
+            self._clients_files_by_id[client_id] = DiskSet(
+                id=f"{self.shard_id}_{client_id}",
+                encode_func=IncomingEdgesFilter.encode,
+                decode_func=IncomingEdgesFilter.decode,
+            )
+        # Store element
+        disk_set = self._clients_files_by_id[client_id]
+        disk_set.add(data)
         return []
 
     def on_eof(self, client_id=None):
-        return []
+        # Check if ID is stored
+        if client_id is None or client_id not in self._clients_files_by_id:
+            return []
+
+        # Iterate over elements
+        disk_set = self._clients_files_by_id[client_id]
+        prev_dest_id = None
+        current_origins = set()
+        for elem in disk_set.elements():
+            dest_id = (elem["To Bank"], elem["Account.1"])
+            # If new key is found, restart counting of intermediaries
+            if prev_dest_id is not None and prev_dest_id != dest_id:
+                # Check condition for sending edges to next stage
+                if len(current_origins) > self._min_incoming:
+                    for origin in current_origins:
+                        yield {
+                            "From Bank" : origin[0],
+                            "Account" : origin[1],
+                            "To Bank" : prev_dest_id[0],
+                            "Account.1" : prev_dest_id[1]
+                        }
+                current_origins.clear()
+            # Add elements to set
+            prev_dest_id = dest_id
+            current_origins.add((elem["From Bank"], elem["Account"]))
+
+        # Check after the loop
+        if prev_dest_id is not None and len(current_origins) > self._min_incoming:
+            for origin in current_origins:
+                yield {
+                    "From Bank" : origin[0],
+                    "Account" : origin[1],
+                    "To Bank" : prev_dest_id[0],
+                    "Account.1" : prev_dest_id[1]
+                }
+
+        del self._clients_files_by_id[client_id]
 
     def _routing_key(self, msg: dict) -> str:
         key = f"{msg['From Bank']}{msg['Account']}"
